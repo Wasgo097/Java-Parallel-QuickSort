c6c7acfe380865cc50f622e58165a3441897e9bd
Wasgo097 <patrpak906@student.polsl.pl>
2020-04-21 21:27:42 +0200

    Done v1

--- src/forkjoin/Main.java
+++ src/forkjoin/Main.java
@@ -9,66 +9,82 @@ public class Main {
             tab[i]=rng.nextInt(1000);
         return tab;
     }
-    public static void main(String[] args) {
-        int[]tab=generate(1500000);
-        int[] tmpArrqs = Arrays.copyOf(tab, tab.length);
-        Sort qsort = new QuickSort();
-        qsort.sort(tmpArrqs);
-        int[] tmpArrpqs = Arrays.copyOf(tab, tab.length);
-        Sort pqsort = new QuickSort();
-        pqsort.sort(tmpArrpqs);
-        for(int i=0;i<tab.length;i++)
-            if(tmpArrqs[i]!=tmpArrpqs[i]){
-                System.out.println("Not this same");
-                break;
+    static int get_best_border(int[]tab){
+        long t1,t2;
+        int border=0;
+        long t=100000000;
+        for(int i=100;i<20000;i+=100){
+            int []temp_arr=Arrays.copyOf(tab, tab.length);
+            Sort pqs=new PQuickSort(i);
+            t1=System.currentTimeMillis();
+            pqs.sort(temp_arr);
+            t2=System.currentTimeMillis();
+            long time=t2-t1;
+            if(time<t){
+                t=time;
+                border=i;
             }
-//        long t1,t2;
-//        {
-//            System.out.println("---SZEREGOWO----");
-//            int[] tmpTab = Arrays.copyOf(tab, tab.length);
-//            Sort sort = new QuickSort();
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 1 "+(t2-t1));
-//            tmpTab = Arrays.copyOf(tab, tab.length);
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 2 "+(t2-t1));
-//            tmpTab = Arrays.copyOf(tab, tab.length);
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 3 "+(t2-t1));
-//        }
-//        {
-//            System.out.println("----RÓWNOLEGLE---");
-//            int[] tmpTab = Arrays.copyOf(tab, tab.length);
-//            Sort sort = new PQuickSort();
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 1 "+(t2-t1));
-//            tmpTab = Arrays.copyOf(tab, tab.length);
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 2 "+(t2-t1));
-//            tmpTab = Arrays.copyOf(tab, tab.length);
-//            t1=System.currentTimeMillis();
-//            sort.sort(tmpTab);
-//            t2=System.currentTimeMillis();
-//            System.out.println("Pomiar 3 "+(t2-t1));
-//        }
-
-//        {
-//            System.out.println("------------------------------------");
-//            int[] tmpTab = Arrays.copyOf(tab, tab.length);
-//            //System.out.println(Arrays.toString(tmpTab));
-//            Sort sort = new PQuickSort();
-//            sort.sort(tmpTab);
-//            //System.out.println(Arrays.toString(tmpTab));
-//        }
+        }
+        System.out.println("Best time is "+t+" for "+border+" border");
+        return border;
+    }
+    static boolean compare(int[]original_array){
+        for(int j=0;j<100;j++) {
+            int[] tmpArrqs = Arrays.copyOf(original_array,original_array.length);
+            int[] tmpArrpqs = Arrays.copyOf(original_array,original_array.length);
+            Sort qsort = new QuickSort();
+            qsort.sort(tmpArrqs);
+            Sort pqsort = new QuickSort();
+            pqsort.sort(tmpArrpqs);
+            for (int i = 0; i < original_array.length; i++)
+                if (tmpArrqs[i] != tmpArrpqs[i]) {
+                    System.out.println("Not this same");
+                    return false;
+                }
+            System.out.println("this same");
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[]original_arr=generate(1500000);
+        long t1,t2;
+        {
+            System.out.println("---SZEREGOWO----");
+            int[] tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            Sort sort = new QuickSort();
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 1 "+(t2-t1));
+            tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 2 "+(t2-t1));
+            tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 3 "+(t2-t1));
+        }
+        {
+            System.out.println("----RÓWNOLEGLE---");
+            int[] tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            Sort sort = new PQuickSort(1700);
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 1 "+(t2-t1));
+            tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 2 "+(t2-t1));
+            tmpTab = Arrays.copyOf(original_arr, original_arr.length);
+            t1=System.currentTimeMillis();
+            sort.sort(tmpTab);
+            t2=System.currentTimeMillis();
+            System.out.println("Pomiar 3 "+(t2-t1));
+        }
     }
 }
